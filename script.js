5fa046074c0fd35da24e040741910eb3194cc190
Emmanuel Charlot <60600965+echarlot1@users.noreply.github.com>
2023-06-30 16:30:36 -0400

    Update script.js
    other

--- script.js
+++ script.js
@@ -78,3 +78,89 @@ const processDirectory = async (dir) => {
     if (err) throw err;
   });
 })();
+
+
+
+
+
+//========================
+
+const fs = require('fs');
+const path = require('path');
+
+function processDirectory(directoryPath) {
+  fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+      console.error(`Error reading directory: ${directoryPath}`, err);
+      return;
+    }
+
+    files.forEach((file) => {
+      const filePath = path.join(directoryPath, file);
+
+      fs.stat(filePath, (err, stats) => {
+        if (err) {
+          console.error(`Error getting file stats: ${filePath}`, err);
+          return;
+        }
+
+        if (stats.isDirectory()) {
+          processDirectory(filePath);
+        } else {
+          processFile(filePath);
+        }
+      });
+    });
+  });
+}
+
+function processFile(filePath) {
+  const fileExtension = path.extname(filePath).toLowerCase();
+
+  if (fileExtension === '.txt') {
+    fs.readFile(filePath, 'utf-8', (err, fileContent) => {
+      if (err) {
+        console.error(`Error reading file: ${filePath}`, err);
+        return;
+      }
+
+      const lines = fileContent.trim().split('\n');
+      const duplicateLines = findDuplicateLines(lines);
+
+      if (duplicateLines.length > 0) {
+        duplicateLines.forEach((line) => {
+          fs.appendFileSync('duplicate_lines.txt', line + '\n', 'utf-8');
+        });
+
+        console.log(`Duplicate lines copied to file: duplicate_lines.txt`);
+      }
+    });
+  }
+}
+
+function findDuplicateLines(lines) {
+  const seen = {};
+  const duplicates = [];
+
+  for (const line of lines) {
+    if (!seen[line]) {
+      seen[line] = true;
+    } else if (!duplicates.includes(line)) {
+      duplicates.push(line);
+    }
+  }
+
+  return duplicates;
+}
+
+// Provide the root directory path here
+const rootDirectory = '/path/to/root/directory';
+const duplicateLinesFile = 'duplicate_lines.txt';
+
+// Delete the duplicate lines file if it already exists
+if (fs.existsSync(duplicateLinesFile)) {
+  fs.unlinkSync(duplicateLinesFile);
+}
+
+processDirectory(rootDirectory);
+
